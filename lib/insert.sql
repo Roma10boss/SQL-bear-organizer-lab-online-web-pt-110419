734fabb78a49dcf52b112a83392a453074185e7b
Romario Gustave <romagustave10@gmail.com>
2020-01-24 00:15:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ;
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES;
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES;
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES;
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES;
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES;
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES;
