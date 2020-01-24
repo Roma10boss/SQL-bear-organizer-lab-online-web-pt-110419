661a1c96433ca4c50ba3f7267c7984ab7f5f8f8c
Romario Gustave <romagustave10@gmail.com>
2020-01-24 00:14:54 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
